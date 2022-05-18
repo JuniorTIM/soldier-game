7f4d615fef3789e43e0d01030f1c97b733190b27
JuniorTIM <musa.aliev06@yandex.ru>
2022-05-18 10:33:21 +0300

    Создан персонаж, заданы основные значения, написаны методы

---
+++ soldier-game.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Джонни',
+    health: 10,
+    gun: {
+        gunName: 'm4',
+        ammo: 30,
+    },
+    supplies: 3,
+
+    shoot: function() {},
+    reload: function() {},
+    wound: function() {},
+}
